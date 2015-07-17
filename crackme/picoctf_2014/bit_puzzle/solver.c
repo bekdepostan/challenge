ac3a383223d8302cb594210c922522a7579a343b
Remi J <remij.android@gmail.com>
2015-07-17 02:05:08 +0200

    [SOLVER] Done

--- crackme/picoctf_2014/bit_puzzle/solver.c
+++ crackme/picoctf_2014/bit_puzzle/solver.c
@@ -16,7 +16,7 @@
 
 #define MAX_DWORD_SIZE 531441
 
-#define INIT_ALL_VAL(i, j, k, l) ( (uint32_t)(INIT_VAL(i) << 24 | INIT_VAL(j) << 16 | INIT_VAL(k) << 8 | INIT_VAL(l)) )
+#define INIT_ALL_VAL(i, j, k, l) ( (INIT_VAL(l) << 24 | INIT_VAL(k) << 16 | INIT_VAL(j) << 8 | INIT_VAL(i)) )
 #define INIT_VAL(x) (x < 26 ? x + 'a' : '_')
 
 typedef struct {
@@ -24,85 +24,93 @@ typedef struct {
 	size_t size;
 } dword_array_t;
 
-bool dword_1_match(uint32_t value) {
-	return (true);
-}
-
-bool dword_2_match(uint32_t value) {
-	/* if (dword_1 + dword_2 == 0x0D5D3DDC) */
-	return (value >= 0x5B000000 && value < 0x75000000);
-}
-
-bool dword_3_match(uint32_t value) {
-	
-	return (value >= 0x4C000000 && value < 0x66000000);
-}
-
-bool dword_4_match(uint32_t value) {
-	
-	return (true);
-}
-bool dword_5_match(uint32_t value) {
-	
-	return (true);
-}
 bool dword_6_match(uint32_t value) {
-	
-	return (value & 0x70000000 == 0x70000000);
-}
-bool dword_7_match(uint32_t value) {
-	// dwprd_7 > dword_6
-	return (true);
-}
 
-bool dword_8_match(uint32_t value) {
-	
-	return (true);
+	return ((value & 0x70000000) == 0x70000000);
 }
-
 
 void init_dword(dword_array_t *dword_n, bool (*filter_match)(uint32_t value))
 {
-	int32_t i, j, k, l, cpt;
-	
+	uint32_t i, j, k, l;
+
 	dword_n->size = 0;
 	for (i = 0; i < 27; i++) {
 		for (j = 0; j < 27; j++) {
 			for (k = 0; k < 27; k++) {
 				for (l = 0; l < 27; l++) {
-					uint32_t value = (uint32_t) INIT_ALL_VAL(i, j, k, l);
+					uint32_t value = INIT_ALL_VAL(i, j, k, l);
 					if (filter_match(value)) {
-						dword_n->array[cpt] = value;
+						dword_n->array[dword_n->size] = value;
 						dword_n->size++;
 					}
 				}
 			}
 		}
 	}
-	printf("initialized %d values\n", dword_n->size);
+	printf("initialized %ld values\n", dword_n->size);
 }
 
 int main(int argc, char *argv[])
 {
-	uint32_t i, j;
-	
-	dword_array_t dword_1;
-	uint32_t dword_2, dword_3;
-	init_dword(&dword_1, dword_1_match);
-	
 
-	for (i = 0; i < dword_1.size; i++) {
-		
-		dword_2 = 0xd5d3dddc - dword_1.array[i];
-		if (dword_2 >= 0x5B000000 && dword_2 < 0x75000000) {
-			dword_3 = 0xc0dcdfce - dword_2;
-			if (dword_3 >= 0x4C000000 && dword_3 < 0x66000000) {
-				if ( (((dword_1.array[i] * 3) & 0xFFFFFFFF) + ((dword_2 * 5) & 0xFFFFFFFF)) & 0xFFFFFFFF == 0x404a7666) {
-					printf("not bad 3\n");
+	dword_array_t dword_6;
+	init_dword(&dword_6, dword_6_match);
+
+	// solving_equations_is_lots_of_fun
+	char *solv_1 = "solv";
+	char *solv_2 = "ing_";
+	char *solv_3 = "equa";
+	char *solv_4 = "tion";
+	char *solv_5 = "s_is";
+	char *solv_6 = "_lot";
+	char *solv_7 = "s_of";
+	char *solv_8 = "_fun";
+
+	uint32_t i;
+
+	for (i = 0; i < dword_6.size; i++) {
+
+		uint32_t dword_7 = dword_6.array[i] - 0x0E000CEC;
+
+		if (dword_6.array[i] / dword_7 == 1 && dword_6.array[i] % dword_7 == 0x0E000CEC) {
+
+			//dword_5 + dword_6 * 2 == 0x5C483831
+			uint32_t dword_5 = 0x5C483831 - (2 * dword_6.array[i]);
+
+			int32_t dword_8 = (0x3726EB17 - (dword_5 * 3)) / 2;
+
+			// (dword_8 * 3) + dword_4 == 0xB9CF9C91)
+			uint32_t dword_4 = 0xB9CF9C91 - (dword_8 * 3);
+
+			// dword_4 ^ dword_1 == 0x18030607
+			uint32_t dword_1 = dword_4 ^ 0x18030607;
+
+			if ((dword_1 & dword_4) == 0x666C6970) {
+
+				uint32_t dword_2 = 0x0D5D3DDDC - dword_1;
+				
+				if ((dword_2 * 5) + (dword_1 * 3) == 0x404A7666) {
+					
+					if (dword_2 * dword_5 == 0xb180902b) {
+
+						//dword_2 + dword_3 == 0x0C0DCDFCE
+						uint32_t dword_3 = 0x0C0DCDFCE - dword_2;
+						
+						if (dword_3 * dword_5 == 0x3E436B5F) {
+						
+							if ((dword_8 * 7) + (dword_3 * 4) == 0x8b0b922d) {
+								printf("flag is: %.*s%.*s%.*s%.*s%.*s%.*s%.*s%.*s", 
+									   4, (char *)&dword_1, 4, (char *)&dword_2,
+									   4, (char *)&dword_3, 4, (char *)&dword_4,
+									   4, (char *)&dword_5, 4, (char *)&dword_6.array[i],
+									   4, (char *)&dword_7, 4, (char *)&dword_8);
+								return(0);
+							}
+						}
+					}
 				}
 			}
 		}
-		
 	}
 	return (0);
 }
